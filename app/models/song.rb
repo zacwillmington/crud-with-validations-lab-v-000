3f3d05589bf780b54e3b354bf1e534541db546a5
Zac Willmington <zac.willmington@gmail.com>
2018-07-09 20:51:29 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,7 +6,7 @@ class Song < ActiveRecord::Base
     # validates :release_year, numericality: { less_than_or_equal_to:  2007 }
     with_options if: :released == true do |year_rel|
           year_rel.validates :release_year, presence: true
-        #  year_rel.validates :release_year, numericality: { less_than_or_equal_to:  2007 }
+          year_rel.validates :release_year, numericality: { less_than_or_equal_to:  2007 }
     end
     validates :artist_name, presence: true
 
